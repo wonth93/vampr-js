eca45d63755e69ee0828fec3742f4f2a42652282
wonth93 <wongth93@gmail.com>
2022-11-02 03:53:07 +0000

    update vampire.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -42,3 +55,15 @@ class Vampire {
 
 module.exports = Vampire;
 
+const original = new Vampire('Original', 1980);
+const ansel = new Vampire('Ansel', 1981);
+const bart = new Vampire('Bart', 1982);
+const elgort = new Vampire('Elgort', 1983);
+const sarah = new Vampire('Sarah', 1984);
+const andrew = new Vampire('Andrew', 1985);
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+elgort.addOffspring(andrew);
